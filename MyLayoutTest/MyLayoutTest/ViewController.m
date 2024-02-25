e5599a539ba174a89ec6e2d4e392def875463396
hs300 <52069334+hs300@users.noreply.github.com>
2024-02-25 17:16:13 +0800

    1,1

--- MyLayoutTest/MyLayoutTest/ViewController.m
+++ MyLayoutTest/MyLayoutTest/ViewController.m
@@ -26,6 +26,45 @@
     container.rightPos.equalTo(@(MyLayoutPos.safeAreaMargin));
     container.backgroundColor = [UIColor greenColor];
     [self.view addSubview:container];
+    
+    MyLinearLayout *layout = [[MyLinearLayout alloc] initWithOrientation:MyOrientation_Vert];
+    layout.gravity = MyGravity_Horz_Center;
+    layout.myHeight = 300;
+    layout.myWidth = MyLayoutSize.fill;
+    layout.backgroundColor = [UIColor blueColor];
+    layout.myBottom = 0;
+    [container addSubview:layout];
+    
+    
+    MyLinearLayout *first = [[MyLinearLayout alloc] initWithOrientation:MyOrientation_Horz];
+    first.backgroundColor = [UIColor orangeColor];
+    first.gravity = MyGravity_Center;
+    first.myHeight = MyLayoutSize.wrap;
+    first.myWidth = MyLayoutSize.wrap;
+    
+    UIView *view = [UIView new];
+    view.myHeight = 80;
+    view.myWidth = 80;
+    view.backgroundColor = [UIColor redColor];
+    [first addSubview:view];
+    
+    [layout addSubview:first];
+    
+    UILabel *uilabel = [UILabel new];
+    uilabel.text = @"xxxxxxxxxxxx   \nxxxx \nx \n xx \noop";
+    uilabel.font = [UIFont systemFontOfSize:16];
+    uilabel.numberOfLines = 0;
+    [uilabel sizeToFit];
+    //[uilabel sizeToFit];
+    [first addSubview:uilabel];
+    
+    MyLinearLayout *sec = [[MyLinearLayout alloc] initWithOrientation:MyOrientation_Horz];
+    
+    sec.backgroundColor = [UIColor yellowColor];
+    sec.myHeight = 80;
+    sec.myWidth = 80;
+    sec.leftPos.equalTo(first.leftPos);
+    [layout addSubview:sec];
 }
 
 
